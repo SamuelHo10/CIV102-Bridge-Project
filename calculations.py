d9a9c2f5721d9de71bb6bd7f69900ba3a916999d
Samuel Ho <samuelho613@gmail.com>
2023-11-02 12:04:13 -0400

    added basic values

---
+++ calculations.py
@@ -0,0 +1,9 @@
+matboard_tensile_strength = 30  # MPa
+matboard_compressive_strength = 6  # MPa
+matboard_shear_strength = 4  # MPa
+matboard_youngs_modulus = 4000  # MPa
+matboard_poissons_ratio = 0.2
+cement_shear_strength = 2  # MPa
+
+
+### Add functions to calculate values here ###
