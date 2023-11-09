7a06d0bc231b289be5381bb0072d3f018a0669a5
Samuel Ho <samuel.is.pathetic@gmail.com>
2023-11-08 22:09:49 -0500

    updated code

--- design0.py
+++ design0.py
@@ -15,27 +15,21 @@ rectangles = [
 ]
 
 
-# ['point' ,length (m), force (N)]
-# ['distributed' , length (m), uniform load (Nm)]
+
+# ['point' ,length (m), force (kN)]
+# ['distributed' , length (m), uniform load (kNm)]
 # ['reaction' , length (m)]
 loads = [
     ["reaction", 0],
-    # ["distributed", 0, 100],
+    ["distributed", 0, calculate.area(rectangles) / 1e6 * calculate.matboard_density],
     ["point", 0.172, 200 / 3],
     ["point", 0.348, 200 / 3],
     ["point", 0.512, 200 / 3],
     ["point", 0.688, 200 / 3],
     ["point", 0.852, 90],
-    ["point", 1.082, 90],
-    # ["distributed", 1.200, 0],
+    ["point", 1.028, 90],
+    ["distributed", 1.200, 0],
     ["reaction", 1.200],
-]
-
-loads2 = [
-    ["reaction", 0],
-    ["distributed", 0, 1.096],
-    ["reaction", 12],
-    ["distributed", 16, 0],
 ]
 
 shear_force_expr, critical_lengths = calculate.get_shear_force_func(loads)
@@ -46,8 +40,6 @@ bending_moment_expr = sy.integrate(shear_force_expr)
 
 plt.plot(bending_moment_expr, (calculate.x, 0, 1.2))
 
-# bending_moment_func = sy.lambdify(calculate.x, bending_moment_expr)(x_array)
-
 max_x, max_y = calculate.highest_bending_moment(critical_lengths, bending_moment_expr)
 
 axis = calculate.centroidal_axis(rectangles)
@@ -56,21 +48,9 @@ second_moment_area = calculate.second_moment_area(rectangles, axis)
 
 bounds = [axis, max([r[0] + r[2] / 2 for r in rectangles]) - axis]
 print(loads)
+print(axis)
 print(bounds, max_y, second_moment_area)
 
 stresses = [b * max_y / second_moment_area *1e3 for b in bounds]
 print( calculate.matboard_tensile_strength / stresses[0])
 print(  calculate.matboard_compressive_strength / stresses[1])
-
-# x_axis, y_axis = calculate.shear_force_data(loads)
-
-# graphs.line_graph(
-#     x_axis, y_axis, "Length (m)", "Shear (N)", save_path="img\\design0_SFD"
-# )
-
-# x_axis, y_axis = calculate.bending_moment_data(loads)
-
-# graphs.line_graph(x_axis, y_axis, "Length (m)", "Bending Moment (Nm)", invert_y=True, save_path="img\\design0_BMD")
-
-# compression, tension = calculate.flexural_stress(rectangles, y_axis)
-# print(f"FOS Compression: {calculate.matboard_compressive_strength/compression}, FOS Tension: {calculate.matboard_tensile_strength/tension}")
