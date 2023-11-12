64d45c13673f23ac47c6773ce54bc540147c52b2
Samuel Ho <samuelho613@gmail.com>
2023-11-11 22:42:17 -0500

    updated graphs

--- graphs.py
+++ graphs.py
@@ -1,25 +1,47 @@
 import numpy as np
-from calculate import x
-# import matplotlib.pyplot as plt
-import sympy.plotting as plt 
+import matplotlib.pyplot as plt
 import sympy as sy
 
-### Add functions to generate graphs here ###
 
-def plot_piecewise(f):
-    # x = sy.Symbol("x", real=True)
-    # x_array = np.linspace(-5, 5, 1000)
-    # f_array = sy.lambdify(x, f)(x_array)
-    print(f)
-    plt.plot( f, (x, 0,16))
+def plot_bridge(nodes, x_bounds=None, y_bounds=None, margin=5):
+    nodes_x = [n.x for n in nodes]
+    nodes_y = [n.y for n in nodes]
+    plt.plot(nodes_x, nodes_y, "o")
+
+    ax = plt.gca()
+    ax.set_aspect("equal", adjustable="box")
+    if x_bounds != None:
+        plt.xlim([x_bounds[0] - margin, x_bounds[1] + margin])
+    if y_bounds != None:
+        plt.ylim([y_bounds[0] - margin, y_bounds[1] + margin])
+    plt.show()
 
 
-def line_graph(y_axis, x_axis_label="x", y_axis_label="y", invert_y=False, save_path="graph.png"):
-    plt.plot(x_axis, y_axis)
+def plot_sympy(
+    sympy_expr,
+    symbol,
+    interval,
+    x_axis_label="x",
+    y_axis_label="y",
+    invert_y=False,
+    save_path="graph.png",
+    spacing=1000,
+    fill=False,
+    show=True,
+):
+    x_axis = np.linspace(interval[0], interval[1], spacing)
+    y_axis = sy.lambdify(symbol, sympy_expr)(x_axis)
+    (line,) = plt.plot(x_axis, y_axis)
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
+    ax = plt.gca()
+    ax.set_xlim(left=interval[0], right=interval[1])
     if invert_y:
-        ax = plt.gca()
         ax.invert_yaxis()
+    if fill:
+        ax.fill_between(x_axis, y_axis, facecolor=line.get_color())
     plt.savefig(save_path)
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        plt.clf()
