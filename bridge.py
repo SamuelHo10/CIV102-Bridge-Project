2f936368e129594c74f009c5e81ce72398fd7240
Samuel Ho <samuel.is.pathetic@gmail.com>
2023-11-12 15:31:22 -0500

    updated bridge code

--- bridge.py
+++ bridge.py
@@ -1,6 +1,24 @@
 from calculate import *
 from graphs import plot_bridge
 
-nodes, top_nodes, bottom_nodes = generate_standard_truss(10,3,50)
-# print(nodes)
-plot_bridge(nodes, y_bounds=[bottom_nodes[0].y,top_nodes[0].y])
+nodes, top_nodes, bottom_nodes = generate_standard_truss(7, 3, 24)
+
+# bottom_nodes[0].force = (0, 0.5)
+# bottom_nodes[-1].force = (0, 0.5)
+# bottom_nodes[len(bottom_nodes) // 2].force = (0, -1)
+bottom_nodes[0].force = (0, 150)
+bottom_nodes[1].force = (0, -60)
+bottom_nodes[2].force = (0, -60)
+bottom_nodes[3].force = (0, -60)
+bottom_nodes[4].force = (0, -60)
+bottom_nodes[5].force = (0, -60)
+bottom_nodes[6].force = (0, 150)
+
+
+members = connect_nodes_pratt(top_nodes, bottom_nodes)
+
+solve_truss(nodes, members)
+
+print(members)
+
+plot_bridge(nodes, members, y_bounds=[bottom_nodes[0].y, top_nodes[0].y])
